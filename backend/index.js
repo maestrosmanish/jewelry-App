bee425dde2e834881a2b3adce630d5f84a02f18b
Manish <maestrosmanish@gmail.com>
2025-09-19 10:16:14 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -65,7 +65,7 @@ app.use((req, res) => {
   });
 
   // Use process.env.PORT for Railway, fallback 3000
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(` Server running on port ${port}`);
   });
